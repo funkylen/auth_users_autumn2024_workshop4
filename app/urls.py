fb697e8edf14791a0dc283a2210d2ed85ac6e14a
funkylen <funkylen@gmail.com>
2024-11-27 14:48:06 +0300

    start from here

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('users', views.users, name='users'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('profile/edit', views.profile_edit, name='profile.edit'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+def users(request):
+    return render(request, 'users.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def logout(request):
+    return redirect(reverse('index') + "?after=logout")
+
+
+def profile_edit(request):
+    return render(request, 'profile/edit.html')
